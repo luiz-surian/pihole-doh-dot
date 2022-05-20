2d6d3b66df75834c4ad20b67a7c766fd53d85011
Luiz Surian <lfsurianfilho@gmail.com>
2022-05-20 20:48:16 -0300

    Update run.sh
    Change from single quotes to double quotes to allow the use of variables, in this case the $(pwd) from the previous commit.

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ if [ IS_RUNNING ]; then
 fi
 
 echo "Running container"
-docker run -d --hostname='pihole-doh-dot' --name='pihole-doh-dot' --cap-add=NET_ADMIN --restart=unless-stopped --net='bridge' --env-file ./config/.env -v '$(pwd)/data/pihole-doh-dot/pihole/':'/etc/pihole/':'rw' -v '$(pwd)/data/pihole-doh-dot/dnsmasq.d/':'/etc/dnsmasq.d/':'rw' -v '$(pwd)/data/pihole-doh-dot/config/':'/config':'rw' -p '53:53/tcp' -p '53:53/udp' -p '1010:80/tcp' 'luiz-surian/pihole-doh-dot:latest'
+docker run -d --hostname="pihole-doh-dot" --name="pihole-doh-dot" --cap-add=NET_ADMIN --restart=unless-stopped --net="bridge" --env-file ./config/.env -v "$(pwd)/data/pihole-doh-dot/pihole/":"/etc/pihole/":"rw" -v "$(pwd)/data/pihole-doh-dot/dnsmasq.d/":"/etc/dnsmasq.d/":"rw" -v "$(pwd)/data/pihole-doh-dot/config/":"/config":"rw" -p "53:53/tcp" -p "53:53/udp" -p "1010:80/tcp" "luiz-surian/pihole-doh-dot:latest"
 
 # Uncomment this if you are confortable using prune to automatically remove unused images.
 #echo "Removing unused images"
