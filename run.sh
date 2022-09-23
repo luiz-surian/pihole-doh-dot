0fd1575fdc8661723ea50795637cd52cd676e03b
Luiz Surian <lfsurianfilho@gmail.com>
2022-09-23 19:01:46 -0300

    Update run.sh
    Remove previous image to force update

--- run.sh
+++ run.sh
@@ -2,23 +2,16 @@
 
 echo -e "\n+=== PiHole DoH/DoT ===+\n"
 
+echo "=== Removing old container ==="
+docker rm -f pihole-doh-dot
+docker rmi lfsurianfilho/pihole-doh-dot:latest
+docker rmi pihole/pihole:latest
+
 echo "=== Building image ==="
+docker pull pihole/pihole:latest
 docker build -t lfsurianfilho/pihole-doh-dot:latest .
 
-IS_RUNNING=$(docker ps -q -f name=pihole-doh-dot)
-if [ IS_RUNNING ]; then
-  echo "=== Stopping container ==="
-  docker stop $IS_RUNNING
-
-  echo "=== Removing container ==="
-  docker rm $IS_RUNNING
-fi
-
 echo "=== Running container ==="
-docker run -d --hostname="pihole-doh-dot" --name="pihole-doh-dot" --cap-add=NET_ADMIN --restart=unless-stopped --net="bridge" --env-file ./config/.env -v "$(pwd)/data/pihole-doh-dot/pihole/":"/etc/pihole/":"rw" -v "$(pwd)/data/pihole-doh-dot/dnsmasq.d/":"/etc/dnsmasq.d/":"rw" -v "$(pwd)/data/pihole-doh-dot/config/":"/config":"rw" -p "53:53/tcp" -p "53:53/udp" -p "1010:80/tcp" "lfsurianfilho/pihole-doh-dot:latest"
-
-# Uncomment this if you are confortable using prune to automatically remove unused images.
-#echo "=== Removing unused images ==="
-#yes | docker image prune -a
+docker run -d --hostname="pihole-doh-dot" --name="pihole-doh-dot" --cap-add=NET_ADMIN --restart=unless-stopped --net="bridge" --env-file ./config/.env -v "$(pwd)/data/pihole/":"/etc/pihole/":"rw" -v "$(pwd)/data/dnsmasq.d/":"/etc/dnsmasq.d/":"rw" -p "53:53/tcp" -p "53:53/udp" -p "1010:80/tcp" "lfsurianfilho/pihole-doh-dot:latest"
 
 echo -e "\n+=== Finished ===+\n"
